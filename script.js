2bf4b1beabb765cea7d2dd7c81f9fc6f00fbad56
dangardiner90 <danieljosephgardiner@gmail.com>
2025-07-12 16:00:16 -0400

    Fixed project button functionality, now togglable

--- script.js
+++ script.js
@@ -1,15 +1,36 @@
 function displayTextOne() {
     var text = document.getElementById("textField1");
-    text.style.display = "block";
+    var button = document.getElementById("project1")
+    if (text.style.display === "none") {
+        text.style.display = "block";
+        button.innerHTML = "Project 1 - Show less"
+    } else {
+        text.style.display = "none";
+        button.innerHTML = "Project 1"
+    }
 };
 
 function displayTextTwo() {
     var text = document.getElementById("textField2");
-    text.style.display = "block";
+    var button = document.getElementById("project2")
+    if (text.style.display === "none") {
+        text.style.display = "block";
+        button.innerHTML = "Project 2 - Show less"
+    } else {
+        text.style.display = "none";
+        button.innerHTML = "Project 2"
+    }
 };
 
 function displayTextThree() {
     var text = document.getElementById("textField3");
-    text.style.display = "block";
+    var button = document.getElementById("project3")
+    if (text.style.display === "none") {
+        text.style.display = "block";
+        button.innerHTML = "Project 3 - Show less"
+    } else {
+        text.style.display = "none";
+        button.innerHTML = "Project 3"
+    }
 };
 
