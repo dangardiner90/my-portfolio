e39151d76d6162f86d8565fb8c1163be94fd1f66
dangardiner90 <danieljosephgardiner@gmail.com>
2025-07-12 15:47:35 -0400

    Added javascript functionality to project section to display project details

---
+++ script.js
@@ -0,0 +1,15 @@
+function displayTextOne() {
+    var text = document.getElementById("textField1");
+    text.style.display = "block";
+};
+
+function displayTextTwo() {
+    var text = document.getElementById("textField2");
+    text.style.display = "block";
+};
+
+function displayTextThree() {
+    var text = document.getElementById("textField3");
+    text.style.display = "block";
+};
+
